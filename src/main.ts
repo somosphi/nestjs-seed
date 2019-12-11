f326fa21c2b08fc7349914bec57394d6d9f11bdd
ciro.azevedo <ciro.azevedo@4all.com>
2019-12-11 16:53:38 -0300

    Add logger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,9 @@ import { HttpExceptionFilter } from './shared/http-exception.filter';
 import { CodedValidatorPipe } from './shared/coded-validation.pipe';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    // logger: Logger,
-  });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new CodedValidatorPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
-
   await app.listen(3000);
 }
 
--- src/user/user.schedule.ts
+++ src/user/user.schedule.ts
@@ -1,17 +1,26 @@
 import { Interval, NestSchedule } from 'nest-schedule';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Injectable()
 export class UserSchedule extends NestSchedule {
-  constructor(private readonly userService: UserService) {
+  private readonly logger = new Logger(UserSchedule.name);
+
+  constructor(
+    private readonly userService: UserService,
+  ) {
     super();
   }
 
   @Interval(1000)
   async runFetch() {
-    const fetchedIds = await this.userService.fetch();
-    // tslint:disable-next-line:no-console
-    console.log({ fetchedIds });
+    try {
+      const fetchedIds = await this.userService.fetch();
+      if (fetchedIds.length) {
+        this.logger.log(`Successfully fetched users [${fetchedIds.join(', ')}]`);
+      }
+    } catch (err) {
+      this.logger.error(`Failed to fetch users (${err.stack})`);
+    }
   }
 }
