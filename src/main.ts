9b443f2c9e6022ca0714634b750ae184ee6a679f
Giuseppe Menti <mentifg@gmail.com>
2019-12-17 15:29:19 -0300

    Add swagger docs generator

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { CodedValidatorPipe } from './shared/coded-validation.pipe';
 import { HttpExceptionFilter } from './shared/http-exception.filter';
@@ -7,6 +8,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new CodedValidatorPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const options = new DocumentBuilder()
+    .setTitle(process.env.npm_package_name)
+    .setDescription(process.env.npm_package_description)
+    .setVersion(process.env.npm_package_version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 
