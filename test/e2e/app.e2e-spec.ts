c4b3c1de0b15e967a7d2c29f1575464cc5466bc2
Giuseppe Menti <mentifg@gmail.com>
2019-12-18 16:03:06 -0300

    Fix tests directories structure

--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -1,16 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../../src/app.module';
 import { ConfigService } from 'src/config/config.service';
-
-const database = new Database(':memory:');
-database.get('SELECT 1', (err, data) => {
-  if (err) {
-    console.log(err.stack);
-    process.exit(0);
-  }
-});
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
