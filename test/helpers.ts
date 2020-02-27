19b4aad996850626b0f9e35abe9e3bd352d87302
Giuseppe Menti <mentifg@gmail.com>
2020-02-27 13:21:26 -0300

    Add method to mock instance for tests

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,13 +1,19 @@
 import * as typeorm from 'typeorm';
 
+type RecursivePartial<T> = {
+  [P in keyof T]?: RecursivePartial<T[P]>;
+};
+
 export const mockTransaction = (repository: typeorm.Repository<any>): void => {
-  jest.spyOn(typeorm, 'getConnection').mockImplementation(() => ({
-    // @ts-ignore
+  const connection = mockInstance<typeorm.Connection>({
     manager: {
       transaction: (cb: any) =>
         cb({
           getCustomRepository: () => repository,
         }),
     },
-  }));
+  });
+  jest.spyOn(typeorm, 'getConnection').mockImplementation(() => connection);
 };
+
+export const mockInstance = <T>(data: RecursivePartial<T>) => data as T;
